09ca2e3166a6fa756eb09d7fe2900a1278a61a7b
reza-rezvani2052 <reza-rezvani2052@gmail.com>
2022-03-19 16:54:38 +0330

    afzudane font be barname

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QFontDatabase>
 
 int main(int argc, char *argv[])
 {
+    Q_INIT_RESOURCE(rc);
+
     QApplication a(argc, argv);
     a.setLayoutDirection(Qt::RightToLeft);
+
+    a.setOrganizationName("");
+    a.setApplicationName("Secure-QR-Code");
+    a.setOrganizationDomain("http://www.google.com");
+    a.setApplicationVersion("1.0.0");
+
+    int id = QFontDatabase::addApplicationFont(":/fonts/iran_sans_web.ttf");
+    QString family = QFontDatabase::applicationFontFamilies(id).at(0);
+    QFont font(family);
+    font.setPointSize( qApp->font().pointSize()+1 );
+    qApp->setFont(font);
 
     MainWindow w;
     w.show();
